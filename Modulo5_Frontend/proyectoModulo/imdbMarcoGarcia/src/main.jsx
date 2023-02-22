1474bce9f71f2eee49d0bb77a4215440616fddc9
Marco Garcia <marcogarcia.gon@gmail.com>
2023-02-22 12:16:42 -0600

    Setting up

---
+++ Modulo5_Frontend/proyectoModulo/imdbMarcoGarcia/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+// Bootstrap
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// Styles
+import './App.css'
+import './styles/styles.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
